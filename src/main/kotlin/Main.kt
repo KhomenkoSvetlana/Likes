1401806bb4729e0874f3a4567caee19eeb18dafb
Svetlana Khomenko <homenko-svetlana87@mail.ru>
2023-06-15 22:05:18 +0300

    Second

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,12 @@
 fun main(args: Array<String>) {
-    val like = 1000
+    val like = 10854
     var result = "лайк"
 
-    if (like < 99) {
-        if (like % 10 in 2..4) {
-            result = "лайка"
-        } else if (like % 10 in 5..9) {
-            result = "лайков"
-        }
-    } else {
-        if (like % 10 in 2..4) {
-            result = "лайка"
-        } else if (like % 100 in 5..14 || like % 10 === 0) {
-            result = "лайков"
-        }
+    if (like % 10 in 2..4) {
+        result = "лайка"
+    } else if (like % 100 in 5..14 || like % 10 in 5..9 || like % 10 === 0) {
+        result = "лайков"
     }
+
     println("У вас $like $result")
 }
