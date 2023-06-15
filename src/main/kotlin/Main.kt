e7cbdd26526dd81833856a9b98760c2fc1ae01c4
Svetlana Khomenko <homenko-svetlana87@mail.ru>
2023-06-15 14:46:36 +0300

    First correction

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,19 @@
 fun main(args: Array<String>) {
-    val like = 100
-    var a = like % 10
+    val like = 1000
     var result = "лайк"
-    if (like % 10 === 2 || like % 10 === 3 || like % 10 === 4 || like % 10 === 5 ) {
-        result = "лайка"
-    } else if (like % 10 === 6 || like % 10 === 7 || like % 10 === 8 || like % 10 === 9 ||  like % 10 === 0) {
-        result = "лайков"
+
+    if (like < 99) {
+        if (like % 10 in 2..4) {
+            result = "лайка"
+        } else if (like % 10 in 5..9) {
+            result = "лайков"
+        }
+    } else {
+        if (like % 10 in 2..4) {
+            result = "лайка"
+        } else if (like % 100 in 5..14 || like % 10 === 0) {
+            result = "лайков"
+        }
     }
-
     println("У вас $like $result")
-
 }
